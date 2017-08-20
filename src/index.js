be9142ca2838b08afe2f228c4a66e326582c92eb
Tom Scott <tscott@weblinc.com>
2017-08-20 00:09:45 -0400

    Rename to dollarsign

---
+++ src/index.js
@@ -0,0 +1,26 @@
+/**
+ * nQuery is a native replacement for jQuery for manipulation of the HTML
+ * DOM.
+ *
+ * @module nquery
+ * @author Tom Scott <tubbo@psychedeli.ca>
+ * @version 0.1.0
+ */
+
+import Dollarsign from 'dollarsign';
+
+export Dollarsign;
+
+/**
+ * Create a new NQuery object using the given selector and scope. If no
+ * scope is provided, use the `document` to get the global page scope.
+ *
+ * @function $
+ * @param {string} selector - Query to run for selecting elements.
+ * @param {EventTarget} scope - DOM to manipulate. Default: `document`.
+ * @return {NQuery} the object created from selector and scope.
+ * @author Tom Scott <tubbo@psychedeli.ca>
+ */
+export default function $(selector) {
+  return new Dollarsign(selector);
+}
