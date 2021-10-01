32f5796e653e0c15ceea2e24718cbcdbda31b438
Tom Scott <tscott@nerdstgamers.com>
2021-10-01 01:45:16 -0400

    style: configure eslint for dom

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,19 @@
-export default {
+module.exports = {
   parser: "babel-eslint",
   extends: ["eslint:recommended"],
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  // TODO: Remove after Jest integration
+  overrides: [
+    {
+      files: ["test/**/*"],
+      globals: {
+        $: true,
+        test: true,
+      },
+    },
+  ],
 };
